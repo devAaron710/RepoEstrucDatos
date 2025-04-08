de39f93a498455ffb10f0963a17bd4c6b047697d
kaaraya212 <148176715+kaaraya212@users.noreply.github.com>
2025-04-07 21:14:26 -0600

    Update EstDatProyecto.java

--- src/estdatproyecto/EstDatProyecto.java
+++ src/estdatproyecto/EstDatProyecto.java
@@ -7,44 +7,42 @@ import javax.swing.JPasswordField;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-public class EstDatProyecto { 
-    
+public class EstDatProyecto {
+
     //credenciales en daatos quemados
-    private static final String userName =  "admin";
-    private static final String password =  "admin";
+    private static final String userName = "admin";
+    private static final String password = "admin";
 
     public static void main(String[] args) throws IOException {
         //varible que mantiene el inicio de sesion en bucle hasta que se ingresen credenciales guardadas
         boolean autenticado = false;
-        
+
         //bucle para solicitar credenciales hasta que sean correctas
         while (!autenticado) {
             //Pedir nombre de usuario
             String user = JOptionPane.showInputDialog("Ingrese su nombre de usuario:");
-            
+
             //si el usuario presiona "Cancelar"
             if (user == null) {
                 JOptionPane.showMessageDialog(null, "Operación cancelada. El sistema se cerrará.");
                 System.exit(0);
             }
-            
+
             //pedir contraseña
             JPasswordField passwordField = new JPasswordField();
             int option = JOptionPane.showConfirmDialog(null, passwordField, "Ingrese su contraseña:", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-            
+
             if (option == JOptionPane.OK_OPTION) {
                 String pass = new String(passwordField.getPassword());
                 //verificar si los datos ingresados coinciden con los datos quemados
                 if (userName.equals(user) && password.equals(pass)) {
                     JOptionPane.showMessageDialog(null, "¡Login exitoso!");
                     //autenticado se actualiza como true
-                    autenticado = true;}
-                
-                else{
+                    autenticado = true;
+                } else {
                     JOptionPane.showMessageDialog(null, "Nombre de usuario o contraseña incorrectos. Intente de nuevo.");
                 }
-            }
-            else{
+            } else {
                 JOptionPane.showMessageDialog(null, "Operación cancelada. El sistema se cerrará.");
                 System.exit(0);
             }
@@ -52,35 +50,37 @@ public class EstDatProyecto {
         //se crea la configuración inicial del banco
         Banco banco = new Banco();
         banco.crearTXT();
-        
+        int cantidadCajas = banco.getCantidadCajas();
         //instancia de objeto cola
-        Cola cola = new Cola(banco);
+        Caja caja = new Caja(cantidadCajas);
+
         //mantiene el menu en bucle hasta que ingrese salir
         boolean continuar = true;
-        
+
         //menu while
         while (continuar) {
             //mostrar menú de opciones
             String opcion = JOptionPane.showInputDialog(
-                    "Seleccione una opción:\n" + 
-                    "1. Crear tiquete\n" +
-                    "2. Atender tiquete\n" +
-                    "3. Mostrar cola\n" +
-                    "4. Mostrar tipo de cambio\n" +
-                    "5. Salir"
+                    "Seleccione una opción:\n"
+                    + "1. Crear tiquete\n"
+                    + "2. Atender tiquete\n"
+                    + "3. Mostrar cola\n"
+                    + "4. Mostrar tipo de cambio\n"
+                    + "5. Ver reportes\n"
+                    + "6. Salir"
             );
-            
+
             //opcion cancelar
-            if (opcion == null){
+            if (opcion == null) {
                 JOptionPane.showMessageDialog(null, "Operación cancelada");
                 System.exit(0);
             }
-            
+
             switch (opcion) {
                 case "1": // Crear tiquete
                     //solicitar datos del tiquete al usuario
                     String nombre = JOptionPane.showInputDialog("Ingrese el nombre del cliente: ");
-                    //.trim()elimina espacios en blanco en los extremos de la cadena
+                    //.trim()elimina espacios en blanco en los extremos de la cadem
                     if (nombre == null || nombre.trim().isEmpty()) {
                         JOptionPane.showMessageDialog(null, "El nombre no puede quedar vacío.");
                         break;
@@ -133,25 +133,31 @@ public class EstDatProyecto {
                     }
 
                     //crear un nuevo tiquete con los datos ingresados
-                    Tiquete tiquete = new Tiquete(nombre, id, edad, tipo, tramite);
+                    Tiquete tiquete = new Tiquete(nombre, id, edad, tipo, tramite, 0);
                     //insertar el tiquete en la cola
-                    cola.insertar(tiquete);
+                    caja.insertarTiquete(tiquete);
                     break;
-                    
-                case "2": //atender tiquete
-                    //atender el primer tiquete y mostrar el estado de la cola
-                    cola.atender();
+
+                case "2": //Atender tiquete
+                    //Solicitar el # de caja para atender el tiquete
+                    String cajaStr = JOptionPane.showInputDialog("Ingrese el numero de caja para atender el tiquete:");
+                    int numeroCaja = Integer.parseInt(cajaStr);
+                    caja.atenderTiquete(numeroCaja);
                     break;
 
                 case "3": //mostrar cola
                     //mostrar el estado de la cola
-                    cola.mostrarCola();
+                    caja.mostrarColas();
                     break;
                 case "4": //mostrar tipo de cambio con webScraping
                     System.out.println(ScrapingWeb.getHTML("https://servicios.davivienda.cr/master/v1/davicotizador/"));
                     new ScrapingWeb().scraping();
                     break;
-                case "5": //salir
+                case "5": //Ver reportes
+                    Reporte reporte = new Reporte();
+                    reporte.consultar(cantidadCajas);
+                    break;
+                case "6": //salir
                     continuar = false;
                     break;
 
